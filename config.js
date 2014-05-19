fe0f8f76cdb9cb8fa763f00e80023c84ba5599a0
Nikita Lvov <niklvov@gmail.com>
2014-05-20 01:15:04 +0400

    start creating db

--- config.js
+++ config.js
@@ -2,6 +2,12 @@ module.exports = {
     http: {
         port: 20302
     },
+    mongo: {
+        user: "developer",
+        pwd: "fwaocbnw3rwctn38ctfgw38x4nt0crtfnzxmg4t30nwct043",
+        url: "37.139.15.10:27017",
+        db: "sblocker"
+    },
     token: {
         google_cert: "https://www.googleapis.com/oauth2/v1/certs",
         aud: "",
