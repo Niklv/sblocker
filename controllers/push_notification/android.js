7d1e7f8ca4d750cf2194b13891618dbbbb9d8ab7
Nikita Lvov <niklvov@gmail.com>
2014-06-20 13:59:51 +0400

    start with android push

--- controllers/push_notification/android.js
+++ controllers/push_notification/android.js
@@ -4,25 +4,42 @@ var config = require('../../config');
 var log = require('../../utils/log')(module);
 
 function pushUpdateMessage() {
-    var opt = {
-        "headers": {
-            "Content-Type": "application/json",
-            "Authorization": "key=AIzaSyCc2_UwqljM_a9BFmRyTa05Wyuh7H-4GIA"
-        },
-        json: {
-            registration_ids: ["ABC", "DEF"],
-            data: {}
+    var msg = {
+        registration_ids: ["ABC"], // this is the device token (phone)
+        collapse_key: "your_collapse_key", // http://developer.android.com/guide/google/gcm/gcm.html#send-msg
+        time_to_live: 180, // just 30 minutes
+        data: {
+            message: "Hello mundo cruel :P" // your payload data
         }
     };
 
-    request.post("https://android.googleapis.com/gcm/send", opt, function (err, res, body) {
-        console.log(err, res.statusCode, body);
-    });
-
-
+    request.post({
+        uri: 'https://android.googleapis.com/gcm/send',
+        json: msg,
+        headers: {
+            Authorization: 'key=' + config.gcm.server_api_key
+        }
+    }, function (err, response, body) {
+        console.log(body);
+    })
 }
 
 module.exports.pushUpdateMessage = pushUpdateMessage;
+
+
+/*var opt = {
+ "headers": {
+ "Authorization": "key=AIzaSyCc2_UwqljM_a9BFmRyTa05Wyuh7H-4GIA"
+ },
+ json: {
+ registration_ids: ["ABC", "DEF"],
+ data: {}
+ }
+ };
+
+ request.post("https://android.googleapis.com/gcm/send", opt, function (err, res, body) {
+ console.log(err, res.statusCode, body);
+ });*/
 
 
 /*var sender = new gcm.Sender(config.gcm.server_api_key);
